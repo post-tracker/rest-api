65a5d938575028175cd1137ada00026a3b86f01c
kokarn <kokarn@gmail.com>
2017-07-04 09:48:33 +0200

    Make development MySQL use a non-root account

--- scripts/vagrant-bootstrap.sh
+++ scripts/vagrant-bootstrap.sh
@@ -14,4 +14,12 @@ apt-get install -y nodejs mysql-server
 # iptables -t nat -I PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 3000
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=localhost" -keyout /vagrant/rest-api/assets/privkey.pem -out /vagrant/rest-api/assets/fullchain.pem
 
+sed -i -e 's/bind-address = 127.0.0.1/#bind-address = 127.0.0.1/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+
+mysql --user="root" --password="root" --execute="CREATE DATABASE devtracker;"
+mysql --user="root" --password="root" --database="devtracker" --execute="CREATE USER 'test'@'%' IDENTIFIED BY 'test';"
+mysql --user="root" --password="root" --execute="GRANT ALL PRIVILEGES ON *.* TO 'test'@'%' IDENTIFIED BY 'test' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
+systemctl restart mysql.service
+
 systemctl status mysql.service
