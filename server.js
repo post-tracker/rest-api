75e66b2ad8b1f44e32a4b021f9dea28b4ef3dbe6
Oskar Risberg <oskar.risberg@stendahls.se>
2018-02-17 14:50:00 +0100

    Add basic robots.txt

--- server.js
+++ server.js
@@ -106,6 +106,10 @@ server.get( '/loaderio-7fa45b57bc0a2a51cd5159425752f4f2/', ( request, response )
     response.sendRaw( 'loaderio-7fa45b57bc0a2a51cd5159425752f4f2' );
 } );
 
+server.get( '/robots.txt', ( request, response ) => {
+    response.sendRaw( 'User-agent: *\r\nAllow: /' );
+} );
+
 server.head( '/:game/posts', ( request, response ) => {
     // Should add game checking
     response.send( SUCCESS_STATUS_CODE );
