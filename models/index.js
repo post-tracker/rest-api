4a48b81ced4495302534b03982d94da62aee41fc
Oskar Risberg <oskar.risberg@stendahls.se>
2022-06-28 15:16:37 +0200

    add some logging

--- models/index.js
+++ models/index.js
@@ -6,6 +6,11 @@ const Sequelize = require( 'sequelize' );
 const env = process.env.NODE_ENV || 'development';
 const config = require( path.join( __dirname, '/../config/config.json' ) )[ env ];
 const models = {};
+config.benchmark = true;
+config.logging = (sql, time /*ms*/) => {
+    console.log( time, sql );
+};
+
 config.host = process.env.DB_HOST;
 
 const sequelize = new Sequelize( 'devtracker', process.env.DB_USERNAME, process.env.DB_PASSWORD, config );
