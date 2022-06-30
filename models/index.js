06dcfbb407d1d4f921f1a04b38a04cfa74c95dfa
Oskar Risberg <oskar.risberg@stendahls.se>
2022-06-30 11:35:27 +0200

    remove benchmarking to fix logs

--- models/index.js
+++ models/index.js
@@ -6,10 +6,10 @@ const Sequelize = require( 'sequelize' );
 const env = process.env.NODE_ENV || 'development';
 const config = require( path.join( __dirname, '/../config/config.json' ) )[ env ];
 const models = {};
-config.benchmark = true;
-config.logging = (sql, time /*ms*/) => {
-    console.log( time, sql );
-};
+// config.benchmark = true;
+// config.logging = (sql, time /*ms*/) => {
+//     console.log( time, sql );
+// };
 
 config.host = process.env.DB_HOST;
 
