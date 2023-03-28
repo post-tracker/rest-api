d9a3e8bc797abaff9883d86d0e0f580d6e78d86c
Oskar Risberg <oskar.risberg@stendahls.se>
2023-03-29 00:09:43 +0200

    revert changes for now

--- models/index.js
+++ models/index.js
@@ -13,7 +13,7 @@ const models = {};
 
 config.host = process.env.DB_HOST;
 
-const sequelize = new Sequelize( 'developertracker', process.env.DB_USERNAME, process.env.DB_PASSWORD, config );
+const sequelize = new Sequelize( 'devtracker', process.env.DB_USERNAME, process.env.DB_PASSWORD, config );
 
 models.Game = sequelize.define(
     'game',
